daad69ff4b773766455d56b9da5ef7645446e870
Ricardo Vargas <r.sagrero1@gmail.com>
2019-09-17 23:05:23 -0500

    Se implemento el uso de dos hilos

--- 1_Practica/main.py
+++ 1_Practica/main.py
@@ -73,35 +73,106 @@ def eliminarAgente(totalAgentes,comunidadAgentes,ipAgentes):
         agentesFile.write("\n")
     agentesFile.close()
 
-def updateAgente(com,host,num):
-    total_input_deliveries = 0
+def updateAgenteComun1(com,host):
     total_output_received= 0
-    print(num, "\n", host)
+
     while True:
         # print("IP_UpdateRRD.py_ ")
+        #Consulta multicast
+        total_input_deliveries = int(
+            consultaSNMP(com, host, "1.3.6.1.2.1.2.2.1.12.5"))
+        valor = "N:" +str(total_input_deliveries)
+        rrdtool.update("Multicast_paquetes_1.rrd", valor)
+        rrdtool.dump("Multicast_paquetes_1.rrd", "Multicast_paquetes_1.xml")
+
+        total_output_deliveries = 0
+        #Consulta IP
+        total_input_deliveries = int(
+            consultaSNMP(com, host,
+                         "1.3.6.1.2.1.4.9.0"))
+        total_output_received = int(
+            consultaSNMP(com, host,
+                         "1.3.6.1.2.1.4.3.0"))
+        valor = "N:" + str(total_input_deliveries) + ':' + str(total_output_received)
+        rrdtool.update("IP_paquetes_1.rrd", valor)
+        rrdtool.dump("IP_paquetes_1.rrd", "IP_update_1.xml")
+
+        total_output_deliveries = 0
+        #Consultas ICMP
+        total_input_deliveries = int(consultaSNMP(com, host,
+                                                  "1.3.6.1.2.1.5.14.0"))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("ICMP_mensajes_1.rrd", valor)
+        rrdtool.dump("ICMP_mensajes_1.rrd", "ICMP_update_1.xml")
+
+        total_output_deliveries = 0
+        #Consulta TCP
+        total_input_deliveries = int(consultaSNMP(com, host,
+                                                  "1.3.6.1.2.1.6.11.0"))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("TCP_segmentos_1.rrd", valor)
+        rrdtool.dump("TCP_segmentos_1.rrd", "TCP_segmentos_1.xml")
+
+        total_output_deliveries = 0
+        #Consulta UDP
+        total_input_deliveries = int(consultaSNMP(com, host,
+                                                  "1.3.6.1.2.1.7.3.0"))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("UDP_datagram_1.rrd", valor)
+        rrdtool.dump("UDP_datagram_1.rrd", "UDP_datagramas_1.xml")
+        time.sleep(1)
+
+
+
+def updateAgenteComun2(com, host, DB, oid, xml):
+    total_output_received = 0
+
+    while True:
+        # print("IP_UpdateRRD.py_ ")
+        # Consulta multicast
+        total_input_deliveries = int(
+            consultaSNMP(com, host, oid))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("Multicast_paquetes_2.rrd", valor)
+        rrdtool.dump("Multicast_paquetes_2.rrd", "Multicast_paquetes_2.xml")
+        total_output_deliveries = 0
+        # Consulta IP
         total_input_deliveries = int(
             consultaSNMP(com, host,
                          '1.3.6.1.2.1.4.9.0'))
         total_output_received = int(
             consultaSNMP(com, host,
                          '1.3.6.1.2.1.4.3.0'))
-
         valor = "N:" + str(total_input_deliveries) + ':' + str(total_output_received)
-        rrdtool.update("IP_paquetes_"+str(num)+".rrd", valor)
-        rrdtool.dump("IP_paquetes_"+str(num)+".rrd", "IP_update_"+str(num)+".xml")
-        time.sleep(1)
-def updateAgenteComun(com,host,DB,oid,xml):
-    total_output_received= 0
+        rrdtool.update("IP_paquetes_2.rrd", valor)
+        rrdtool.dump("IP_paquetes_2.rrd", "IP_update_2.xml")
 
-    while True:
-        # print("IP_UpdateRRD.py_ ")
-        total_input_deliveries = int(
-            consultaSNMP(com, host,oid))
+        total_output_deliveries = 0
+        # Consultas ICMP
+        total_input_deliveries = int(consultaSNMP(com, host,
+                                                  "1.3.6.1.2.1.5.14.0"))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("ICMP_mensajes_2.rrd", valor)
+        rrdtool.dump("ICMP_mensajes_2.rrd", "ICMP_update_2.xml")
 
-        valor = "N:" +str(total_output_received)
-        rrdtool.update(DB, valor)
-        rrdtool.dump(DB, xml)
+        total_output_deliveries = 0
+        # Consulta TCP
+        total_input_deliveries = int(consultaSNMP(com, host,
+                                                  "1.3.6.1.2.1.6.11.0"))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("TCP_segmentos_2.rrd", valor)
+        rrdtool.dump("TCP_segmentos_2.rrd", "TCP_segmentos_2.xml")
+
+        total_output_deliveries = 0
+        # Consulta UDP
+
+        total_input_deliveries = int(consultaSNMP(com, host,
+                                                  "1.3.6.1.2.1.7.3.0"))
+        valor = "N:" + str(total_input_deliveries)
+        rrdtool.update("UDP_datagram_2.rrd", valor)
+        rrdtool.dump("UDP_datagram_2.rrd", "UDP_datagramas_2.xml")
         time.sleep(1)
+
 
 def crearGraficaDoble(segundos, nombreImagen, etiquetaVertical, tituloTabla, DB):
     tiempo_actual = int(time.time())
@@ -147,67 +218,22 @@ while True:
             print("Agente ",i+1,"=",result, "Up")
             inicioAgente(comunidadAgentes[i], ipAgentes[i])
             # Para este agente se selecciono la interfaz 5
-            t1 = threading.Thread(name="hilo_Multicast_1", target=updateAgenteComun, args=(comunidadAgentes[i],
-                                                                                         ipAgentes[i],
-                                                                                         "Multicast_paquetes_1.rrd",
-                                                                                         "1.3.6.1.2.1.2.2.1.12.5",
-                                                                                           "Multicast_paquetes_1.xml"))
-            t2 = threading.Thread(name="hilo_IP_Agente_1", target=updateAgente, args=(comunidadAgentes[i],
-                                                                                             ipAgentes[i],
-                                                                                      1))
-            t3 = threading.Thread(name="hilo_ICMP_Agente_1", target=updateAgenteComun, args=(comunidadAgentes[i],
-                                                                                                ipAgentes[i],
-                                                                                                  "ICMP_mensajes_1.rrd",
-                                                                                                  "1.3.6.1.2.1.5.14.0",
-                                                                                                  "ICMP_update_1.xml"))
-            t4 = threading.Thread(name="hilo_TCP_Agente_1", target=updateAgenteComun, args=(comunidadAgentes[i],
-                                                                                                 ipAgentes[i],
-                                                                                                 "TCP_segmentos_1.rrd",
-                                                                                                 "1.3.6.1.2.1.6.11.0",
-                                                                                                 "TCP_segmentos_1.xml"))
-            t5 = threading.Thread(name="hilo_UDP_Agente_1", target=updateAgenteComun, args=(comunidadAgentes[i],
-                                                                                                 ipAgentes[i],
-                                                                                                 "UDP_datagram_1.rrd",
-                                                                                                 "1.3.6.1.2.1.7.3.0",
-                                                                                                 "UDP_datagramas_1.xml"))
+
+            t1 = threading.Thread(name="hilo_TCP_Agente_1", target=updateAgenteComun1,
+                                  args=(comunidadAgentes[i],
+                                        ipAgentes[i]))
             # Segundo agente, se selecciona la interfaz 9 para la primera pregunta
-            t6 = threading.Thread(name="hilo_Multicast_2", target=updateAgenteComun, args=(comunidadAgentes[1],
-                                                                                           ipAgentes[1],
-                                                                                           "Multicast_paquetes_2.rrd",
-                                                                                           "1.3.6.1.2.1.2.2.1.12.5",
-                                                                                           "Multicast_paquetes_2.xml"))
-            t7 = threading.Thread(name="hilo_IP_Agente_2", target=updateAgente, args=(comunidadAgentes[1], ipAgentes[1],
-                                                                                      2))
-            t8 = threading.Thread(name="hilo_ICMP_Agente_2", target=updateAgenteComun,
-                                  args=(comunidadAgentes[1],
-                                        ipAgentes[1],
-                                        "ICMP_mensajes_2.rrd",
-                                        "1.3.6.1.2.1.5.14.0",
-                                        "ICMP_update_2.xml"))
-            t9 = threading.Thread(name="hilo_TCP_Agente_2", target=updateAgenteComun,
+            t2 = threading.Thread(name="hilo_TCP_Agente_2", target=updateAgenteComun2,
                                   args=(comunidadAgentes[1],
                                         ipAgentes[1],
                                         "TCP_segmentos_2.rrd",
                                         "1.3.6.1.2.1.6.11.0",
                                         "TCP_segmentos_2.xml"))
-            t10 = threading.Thread(name="hilo_UDP_Agente_2", target=updateAgenteComun,
-                                  args=(comunidadAgentes[1],
-                                        ipAgentes[1],
-                                        "UDP_datagram_2.rrd",
-                                        "1.3.6.1.2.1.7.3.0",
-                                        "UDP_datagramas_2.xml"))
+
             # Con esta funcion estamos activando los hilos de dos agentes estaticos
-            if not t1.is_alive():
+            if not t1.is_alive() and not t2.is_alive():
                 t1.start()
                 t2.start()
-                t3.start()
-                t4.start()
-                t5.start()
-                t6.start()
-                t7.start()
-                t8.start()
-                t9.start()
-                t10.start()
         else:
             print("Agente ",i+1, "= Down")
     agentesFile.close()
@@ -226,9 +252,9 @@ while True:
                           "IP_paquetes_1.rrd")
         crearGraficaSencilla(segundos, "ICMP_mensajes_1.png", "Mensajes", "Mensajes de respuesta ICMP",
                              "ICMP_mensajes_1.rrd")
-        crearGraficaSencilla(segundos, "TCP_segmentos_1.png", "Segmentos", "Segmentos enviados",
+        crearGraficaSencilla(segundos, "TCP_segmentos_1.png", "Segmentos", "Segmentos enviados TCP",
                              "TCP_segmentos_1.rrd")
-        crearGraficaSencilla(segundos, "UDP_datagramas_1.png", "Datagramas","Datagramas recibidos que no puedieron",
+        crearGraficaSencilla(segundos, "UDP_datagramas_1.png", "Datagramas","Datagramas recibidos que no puedieron UDP",
                              "UDP_datagram_1.rrd")
         # Segunda
         crearGraficaSencilla(segundos, "Multicast_paquetes_2.png", "Trafico de RED Multicast recibido",
@@ -237,9 +263,9 @@ while True:
                           "IP_paquetes_2.rrd")
         crearGraficaSencilla(segundos, "ICMP_mensajes_2.png", "Mensajes", "Mensajes de respuesta ICMP",
                              "ICMP_mensajes_2.rrd")
-        crearGraficaSencilla(segundos, "TCP_segmentos_2.png", "Segmentos", "Segmentos enviados",
+        crearGraficaSencilla(segundos, "TCP_segmentos_2.png", "Segmentos", "Segmentos enviados TCP",
                              "TCP_segmentos_2.rrd")
-        crearGraficaSencilla(segundos, "UDP_datagramas_2.png", "Datagramas", "Datagramas recibidos que no puedieron",
+        crearGraficaSencilla(segundos, "UDP_datagramas_2.png", "Datagramas", "Datagramas recibidos que no puedieron UDP",
                              "UDP_datagram_2.rrd")
         crearPDF(comunidadAgentes[0],ipAgentes[0],1)
         crearPDF(comunidadAgentes[1],ipAgentes[1],2)
